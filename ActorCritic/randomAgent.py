e3d611230495bb02f6da861f1613e88d4a31b4c6
Yassine Bareche <fy_bareche@esi.dz>
2021-05-11 14:31:52 +0200

    Actor Critic policy gradient

---
+++ ActorCritic/randomAgent.py
@@ -0,0 +1,117 @@
+import argparse
+import sys
+import matplotlib
+#matplotlib.use("Qt5agg")
+matplotlib.use("TkAgg")
+import gym
+import gridworld
+import torch
+from utils import *
+from torch.utils.tensorboard import SummaryWriter
+
+
+
+class RandomAgent(object):
+    """The world's simplest agent!"""
+
+    def __init__(self, env, opt):
+        self.opt=opt
+        self.env=env
+        if opt.fromFile is not None:
+            self.load(opt.fromFile)
+        self.action_space = env.action_space
+        self.featureExtractor = opt.featExtractor(env)
+
+    def act(self, observation, reward, done):
+        return self.action_space.sample()
+
+    def save(self,outputDir):
+        pass
+
+    def load(self,inputDir):
+        pass
+
+
+if __name__ == '__main__':
+    #config = load_yaml('./configs/config_random_gridworld.yaml')
+    config = load_yaml('./configs/config_random_cartpole.yaml')
+    #config = load_yaml('./configs/config_random_lunar.yaml')
+
+    freqTest = config["freqTest"]
+    freqSave = config["freqSave"]
+    nbTest = config["nbTest"]
+
+    env = gym.make(config["env"])
+    if hasattr(env, 'setPlan'):
+        env.setPlan(config["map"], config["rewards"])
+
+    tstart = str(time.time())
+    tstart = tstart.replace(".", "_")
+    outdir = "./XP/" + config["env"] + "/random_" + "-" + tstart
+
+
+    env.seed(config["seed"])
+    np.random.seed(config["seed"])
+    torch.manual_seed(config["seed"])
+
+    episode_count = config["nbEpisodes"]
+    ob = env.reset()
+
+    agent = RandomAgent(env,config)
+
+    print("Saving in " + outdir)
+    os.makedirs(outdir, exist_ok=True)
+    save_src(os.path.abspath(outdir))
+    write_yaml(os.path.join(outdir, 'info.yaml'), config)
+    logger = LogMe(SummaryWriter(outdir))
+    loadTensorBoard(outdir)
+
+    rsum = 0
+    mean = 0
+    verbose = True
+    itest = 0
+    reward = 0
+    done = False
+    for i in range(episode_count):
+        if i % int(config["freqVerbose"]) == 0 and i >= config["freqVerbose"]:
+            verbose = True
+        else:
+            verbose = False
+
+        if i % freqTest == 0 and i >= freqTest:  ##### Same as train for now
+            print("Test time! ")
+            mean = 0
+            agent.test = True
+
+        if i % freqTest == nbTest and i > freqTest:
+            print("End of test, mean reward=", mean / nbTest)
+            itest += 1
+            logger.direct_write("rewardTest", mean / nbTest, itest)
+            agent.test = False
+
+        if i % freqSave == 0:
+            agent.save(outdir + "/save_" + str(i))
+
+        j = 0
+        if verbose:
+            env.render()
+
+        while True:
+            if verbose:
+                env.render()
+
+            action = agent.act(ob, reward, done)
+            ob, reward, done, _ = env.step(action)
+            j+=1
+
+            rsum += reward
+            if done:
+                print(str(i) + " rsum=" + str(rsum) + ", " + str(j) + " actions ")
+                logger.direct_write("reward", rsum, i)
+                agent.nbEvents = 0
+                mean += rsum
+                rsum = 0
+                ob = env.reset()
+                break
+
+    env.close()
--- DQN_Actor_Critic/gridworld2/__init__.py
+++
@@ -1,8 +0,0 @@
-from gridworld2.gridworld_env import GridworldEnv
-
-from gym.envs.registration import register
-
-register(
-    id='gridworld-v1',
-    entry_point='gridworld2:GridworldEnv',
-)
